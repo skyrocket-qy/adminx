ac060a4dc9738955865eb9aa58d79df0e00651a2
Jimmy Huang <rivendinner@gmail.com>
2025-04-15 03:02:57 +0800

    try

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
   output: 'export',
-  basePath: '/adminx',
-  assetPrefix: '/adminx',
+  // basePath: '/adminx',
+  // assetPrefix: '/adminx',
+  images: {
+    unoptimized: true, // ✅ Add this
+  },
 };
 
 export default nextConfig;
