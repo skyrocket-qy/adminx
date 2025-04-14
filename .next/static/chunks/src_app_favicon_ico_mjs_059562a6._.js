dab89050a35b8cae9746b375b6ffd7f529ba50a5
Jimmy Huang <rivendinner@gmail.com>
2025-04-15 01:37:50 +0800

    rm .next

--- .next/static/chunks/src_app_favicon_ico_mjs_059562a6._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/75d31_next_dist_fd2f0fda._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_layout_tsx_a40108e8._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/75d31_next_dist_92f66d94._.js",
-  "static/chunks/src_app_components_header_tsx_846eb9e2._.js"
-],
-    source: "dynamic"
-});
